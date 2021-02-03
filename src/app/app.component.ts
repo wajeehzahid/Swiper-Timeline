b44a6c57639adb934a82c24328e8e93b5cd7cb01
wajeehzahid <wajeeh_zahid@hotmail.com>
2021-02-03 20:46:18 +0500

    made swiper timeline on Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+// import Swiper core and required modules
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+} from 'swiper/core';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-root',
@@ -7,4 +17,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Swiper-Timeline';
+
+  slideData = [
+    {
+      date: 'March 1999',
+      content: 'Tamra Rent A Cat is officially registered as a car rental agency on Jeju Island with its CEO being In-woong Park.'
+    },
+    {
+      date: 'January 2002',
+      content: 'Tamra has 120 vehicles registered and opens its first administration office in Seogwipo.'
+    },
+    {
+      date: 'April 2003',
+      content: 'Tamra\'s head office is moved to Jeju City and a sales office is opened in Gwangju.'
+    },
+  ];
 }
